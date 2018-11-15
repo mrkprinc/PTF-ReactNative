744b0963c0251c786ff013136f316b3a8fdae081
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-11-14 23:49:03 -0500

    implement redux

--- app/components/App.js
+++ app/components/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { View } from 'react-native';
 import Text from './Text';
 import NavBar from './Navbar';
 import styles from '../styles/_components/App';
+import rootReducer from '../reducers';
+
+const store = createStore(rootReducer);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This will be the Press the Freedom app.</Text>
-        <NavBar />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>This will be the Press the Freedom app.</Text>
+          <NavBar />
+        </View>
+      </Provider>
     );
   }
 }
